010c216a6d05b3d1ab847024ed4dba017dcd7e9d
thamarais16 <thamarais16@gmail.com>
2021-04-30 19:57:11 +0530

    2 files modified

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-followers.component.css']
 })
 export class GithubFollowersComponent implements OnInit {
-
+  followers: Follower<string, number>[];
+  
   constructor() { }
 
   ngOnInit() {
+    this.followers = [
+      new Follower("Thamarai",1),
+      new Follower("Selvan", 2)
+    ]
   }
 
+}
+
+class Follower<T, U>{
+  name: T;
+  id: U
+  constructor(name: T, id: U){
+    this.name = name;
+    this.id = id;
+  }
 }
